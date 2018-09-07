bab8f68ca9cbe1e8d75377ff4070769e8c183c70
Stuart Homfray <stuart.homfray@w3w.cz>
2018-09-07 14:42:53 +0200

    setup service worker caching (NOTE sw.js will not work here)

--- example/assets/js/main.js
+++ example/assets/js/main.js
@@ -50,3 +50,17 @@ async function onOkButtonClickAsync() {
         console.error(`error ${err}`);
     }
 }
+
+/**
+ * Install the service worker
+ */
+async function installServiceWorkerAsync() {
+    if ('serviceWorker' in navigator) {
+        try {
+            let serviceWorker = await navigator.serviceWorker.register('/sw.js')
+            console.log(`Service worker registered ${serviceWorker}`)
+        } catch (err) {
+            console.error(`Failed to register service worker: ${err}`)
+        }
+    }
+}
